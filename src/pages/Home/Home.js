d4c7ee679ebe23a6af26a46b2b90d1a884cdb22a
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-03-05 17:57:19 +0530

    Completed Home section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,35 @@
 import React from 'react'
 import './Home.css'
+import Typewriter from 'typewriter-effect';
+import Resume from '../../asssets/docs/Solution.pdf'
+import Hired from '../../asssets/docs/Question.pdf'
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <>
+      <div className='container-fluid home-container'>
+        <div className='container home-content'>
+          <h1>Hi I'm a</h1>
+          <h2>
+          <Typewriter
+            options={{
+              strings: [
+                'FullStack Developer !',
+                'Mern Stack Developer !'],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+
+          </h2>
+          <div className='home-buttons'>
+            <a className='btn btn-hire' href={Hired} download="Your_cv.pdf">Hire Me</a>
+            <a className='btn btn-cv' href={Resume} download="Your_name.pdf">My Resume</a>
+
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
