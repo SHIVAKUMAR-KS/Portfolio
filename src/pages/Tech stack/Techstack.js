6065a2f410d087f795a8a9a2dd03b92905106508
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-03-06 17:40:16 +0530

    updated home page

--- src/pages/Tech stack/Techstack.js
+++ src/pages/Tech stack/Techstack.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Techstack.css'
+import { TechstackList } from '../../utils/TechstackList'
 
 const Techstack = () => {
   return (
@@ -8,8 +9,30 @@ const Techstack = () => {
         <h2 className='col-12 mt-3 mb-1 text-center'>Technologies Stack</h2>
         <hr/>
         <p className='pb-3 text-center'>❤️❤️ including program language,framework,database,frontend,backend and API's</p>
-
+        <div className="row">
+          {TechstackList.map((tech) => (
+            
+              <div key={tech._id} className="col-md-3">
+                <div className="card m-2">
+                  <div className="card-content">
+                    <div className="card-body">
+                      <div className="media d-flex justify-content-center">
+                        <div className="alig-self-center">
+                          <tech.icon className="tech-icon" />
+                        </div>
+                        <div className="media-body">
+                          <h5>{tech.name}</h5>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            
+          ))}
+        </div>
       </div>
+      
     </>
   )
 }
