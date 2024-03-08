cecd26b903e4d9d52a8b2cfc32c4345f1e0e3c26
shiva kumar <115865333+SHIVAKUMAR-KS@users.noreply.github.com>
2024-03-08 22:35:06 +0530

    Added email funtion in form Contact.js

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,88 +1,94 @@
-import React from 'react'
-import './Contact.css'
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
-
+import React from "react";
+import "./Contact.css";
+import { BsFacebook, BsGithub, BsLinkedin } from "react-icons/bs";
 const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "4eaa7b0e-cc5c-4695-9274-79b54a4075cb");
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log("Success", res);
+      setResult(res.message);
+      event.target
+      .reset();
+    } else {
+      console.log("Error", res);
+      setResult(res.message);
+    }
+  };
+
+
+
   return (
     <>
-    <div className='contact' id='contact'>
-        <div className='card card0 border-0'>
-            <div className='row'>
-                <div className='col-md-c ol-lg-6 col-xk-6 col-sm-12'>
-                    <div className='card1'>
-                        <div className='row border-line'>
-                            <img src='https://img.freepik.com/free-photo/hot-line-contact-us-call-center-search-interface_53876-124009.jpg' alt='contact'/>
-
-                        </div>
-
-                    </div>
-
+      <div className=" contact">
+        <div className="card card0 border-0">
+          <div className="row">
+            <div className="col-md-6 col-lg-6 col-xl-6 col-sm-12">
+              <div className="card1">
+                <div className="row border-line">
+                  <img
+                    src="https://img.freepik.com/free-photo/hot-line-contact-us-call-center-search-interface_53876-124009.jpg?w=2000"
+                    alt="ocontact"
+                    className="image"
+                  />
                 </div>
-                <div className='col-lg-6 col-md-6'>
-                    <div className='row'>
-                        <div className='card2 d-flex card border-0 px-4 py-5'>
-                            <h6>Contact With 
-                            <FaLinkedin color='blue' size={30}className='ms-2'/>
-                            <FaGithub color='black' size={30}className='ms-2'/>
-                            <FaFacebook color='blue' size={30}className='ms-2'/>
-                            </h6>
-
-                        </div>
-                        <div className='row px-3 mb-4'>
-                            <div className='line'/>
-                            <small className='pr text-center'>OR</small>
-                            <div className='line'/>
-
-                        </div>
-                        <div className='row px-3'>
-                            <input 
-                            type='text'
-                            name='name' 
-                            placeholder='write your Name' 
-                            className='mb-3'/>
-
-                        </div>
-                        <div className='row px-3'>
-                            <input 
-                            type='email'
-                            name='email' 
-                            placeholder='enter your emil' 
-                            className='mb-3'/>
-
-                        </div>
-                        <div className='row px-3'>
-                            <textarea
-                            type='text'
-                            name='name' 
-                            placeholder='write your Name' 
-                            className='mb-3'
-                            
-                            />
-                            
-
-                        </div>
-                        <div className='row px-3'>
-                            <button className='button' type='submit'>
-                                send message
-                            </button>
-                            
-
-                        </div>
-
-
-                    </div>
-
-                </div>
-
+              </div>
             </div>
+            <div className="col-lg-6 col-md-6">
+              <div className="card2 d-flex card border-0 px-4 py-5">
+                <div className="row" onSubmit={onSubmit}>
+                  <div className="row">
+                    <h6>
+                      Contact With
+                      <BsLinkedin color="blue" size={30} className="ms-2" />
+                      <BsGithub color="black" size={30} className="ms-2" />
+                      <BsFacebook color="blue" size={30} className="ms-2" />
+                    </h6>
+                  </div>
+
+                  <div className="row px-3 mb-4">
+                    <div className="line" />
+                    <small className="or text-center">OR</small>
+                    <div className="line" />
+                  </div>
+                  
+                  <div className="contact-col">
+            <form onSubmit={onSubmit}>
+                <label>Your name</label>
+                <input type="text" name='name' placeholder='Enter your name' required />
+
+                <label>Phone Number</label>
+                <input type="tel" name='phone' placeholder='Enter your mobil number' required/>
+
+                <label >Write Your message here</label>
+                <textarea name="message" rows="6" placeholder='Enter your message' required></textarea>
+
+                <button type='submit' className='btn dark-btn'>Submit now <img src='' alt="" /></button>
+            </form>
+
+            <span>{result}</span>
 
         </div>
-
-    </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
