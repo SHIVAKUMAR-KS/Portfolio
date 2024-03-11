05dffc35f9f0f1540362056a17015ee62b7f29d5
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-03-11 07:38:23 +0530

    updated education

--- src/pages/Education/Education.js
+++ src/pages/Education/Education.js
@@ -3,6 +3,7 @@ import { IoIosSchool } from "react-icons/io";
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import './Education.css'
+import { FaSchool } from "react-icons/fa";
 
 const Education = () => {
   return (
@@ -18,27 +19,27 @@ const Education = () => {
         contentStyle={{ background: 'rgb(33, 150, 243)', color: 'black' }}
         contentArrowStyle={{ borderRight: '7px solid  white' }}
         date="2022 - 2026"
-        iconStyle={{ background: "#138781", color: '#fff' }}
+        iconStyle={{ background: "#083452", color: '#fff' }}
         icon={<IoIosSchool />}
         >
         <h3 className="vertical-timeline-element-title">Btech</h3>
         <h4 className="vertical-timeline-element-subtitle">CMR Technical Campus</h4>
         <p>
-        Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+        Learning Computer Science,Creative Direction, User Experience, Visual Design, Project Management, Team Leading
         </p>
-    </VerticalTimelineElement>
+       </VerticalTimelineElement>
             <VerticalTimelineElement
         className="vertical-timeline-element--work"
         contentStyle={{ background: 'rgb(33, 150, 243)', color: 'black' }}
         contentArrowStyle={{ borderRight: '7px solid  white' }}
         date="2020 - 2022"
         iconStyle={{ background: "#138781", color: '#fff' }}
-        icon={<IoIosSchool />}
+        icon={<FaSchool />}
         >
         <h3 className="vertical-timeline-element-title">Intermediate</h3>
         <h4 className="vertical-timeline-element-subtitle">Holy Mission school</h4>
         <p>
-        Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+        Creative Direction, Team Leading
         </p>
     </VerticalTimelineElement>
         </VerticalTimeline>
